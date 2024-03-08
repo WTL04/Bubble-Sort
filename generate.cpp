a707e5ea69995a0068675e0ff335ab1c392805d0
WTL04 <williamluu04@gmail.com>
2024-03-08 14:55:05 -0800

    Generate.cpp working

--- generate.cpp
+++ generate.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <cstdlib> //for rand
 #include <ctime> //for time
+#include <string>
 
 using namespace std;
 
@@ -24,20 +25,43 @@ int main(int argc, char* argv[])  //argc: keeps count of # of arguments, argv: t
     int min = stoi(argv[2]);
     int max = stoi(argv[3]);
 
+    cout << "argv[0]: " << argv[0] << endl;
+    cout << "argv[1]: " << count << endl;
+    cout << "argv[2]: " << min << endl;
+    cout << "argv[3]: " << max << endl;
+
     if (argc != 4)
     {
         cout << "Error: Not enough arguments" << endl;
     }
 
-    ofstream file("numbers.dat");
+    ofstream file;
+    file.open("numbers.dat", ios::trunc); //opens file and clears it out
     if (!file)
     {
         cout << "Error: file not found" << endl;
     }
-    
+    file.close();
+
+    //reopened file
+    file.open("numbers.dat", ios::app); //opens file in append mode
+    if (!file)
+    {
+        cout << "Error: file not found" << endl;
+    }
+
+
     //seed rand() time
     srand(time(nullptr));
-    // testing
+
+    for (int i = min; i < max; i++)
+    {
+        int randInt = generateRandInt(min, max);
+        file << randInt << " ";
+    }
+
+    file.close();
+
 
     return 0;
 }
